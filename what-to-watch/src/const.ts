22d96a9dd6679d87d630781f73bdfea3222e8679
Elena <eamatytsina@gmail.com>
2024-11-04 13:46:11 +0300

    basic screenss are  created, filmCard component is created

---
+++ what-to-watch/src/const.ts
@@ -0,0 +1,4 @@
+export const Setting = {
+    favouriteFilmsCount : 3,
+    filmsCardsCount: 20
+}
--- what-to-watch/src/main.tsx
+++ what-to-watch/src/main.tsx
@@ -2,9 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Setting } from './const.ts';
+
+const DEFAULT_FILM_TITLE = "The Grand Budapest Hotel";
+const DEFAULT_GENRE = "Drama";
+const DEFAULT_YEAR = 2014;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <App
+      favouriteFilmsCount={Setting.favouriteFilmsCount}
+      filmsCardsCount={Setting.filmsCardsCount}
+      promoFilmTitle={DEFAULT_FILM_TITLE}
+      promoFilmGenre={DEFAULT_GENRE}
+      promoFilmIssueYear={DEFAULT_YEAR}
+
+    />
   </StrictMode>,
 )
